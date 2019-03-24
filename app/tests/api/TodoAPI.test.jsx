eb9180a76e553c313db3aba544b75fb2545b173c
ondra Ke <ondra.to.stejne.necte@seznam.cz>
2019-03-24 10:06:01 +0100

    api test

---
+++ app/tests/api/TodoAPI.test.jsx
@@ -0,0 +1,51 @@
+var expect = require('expect');
+
+var TodoAPI = require('TodoAPI');
+
+describe('TodoAPI', () => {
+    beforeEach(()=>{
+        localStorage.removeItem('todos');
+    });
+    it('should exist', () => {
+        expect(TodoAPI).toExist();
+    });
+
+    describe('setTodos', () => {
+        it('should set valid array', () => {
+            var todos = [{
+                id: 23,
+                test: 'test all files',
+                completed: false
+            }];
+            TodoAPI.setTodos(todos);
+
+            var actualTodos = JSON.parse(localStorage.getItem('todos'));
+            expect(actualTodos).toEqual(todos);
+        });
+
+        it('should not set invalid array', () =>{
+            var badTodos = {a: 'b'};
+            TodoAPI.setTodos(badTodos);
+            expect(localStorage.getItem('todos')).toBe(null);
+        });
+    });
+
+    describe('getTodos', () => {
+        it('should get empty array for bad local storage data', () => {
+            var actualTodos = TodoAPI.getTodos();
+            expect(actualTodos).toEqual([]);
+        });
+
+        it('should get valid array', () => {
+            var todos = [{
+                id: 23,
+                test: 'test all files',
+                completed: false
+            }];
+            localStorage.setItem('todos', JSON.stringify(todos));
+
+            var actualTodos = TodoAPI.getTodos();
+            expect(actualTodos).toEqual(todos);
+        });
+    });
+});
